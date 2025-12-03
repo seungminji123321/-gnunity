a99de864fcc9fb3a615a9b06f4cf20a8d2eb04ca
지승민 <gkstls806@gmail.com>
2025-12-03 13:06:53 +0900

    Update club_post_detail_screen.dart

--- lib/screens/club_post_detail_screen.dart
+++ lib/screens/club_post_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gnunity/models/user_model.dart';
 import 'package:share_plus/share_plus.dart'; // share_plus 패키지 import
 
 class ClubPostDetailScreen extends StatefulWidget {
@@ -162,4 +163,5 @@ class _ClubPostDetailScreenState extends State<ClubPostDetailScreen> {
       ),
     );
   }
+
 }
